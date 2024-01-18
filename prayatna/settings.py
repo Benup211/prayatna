66b22db880915edf79da6955df597b461d6ca696
benup211 <benup211@gmail.com>
2024-01-18 23:56:39 +0545

    postgresql database added

--- prayatna/settings.py
+++ prayatna/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'prayatna.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prayatna',
+        'USER': 'benup',
+        'PASSWORD': 'Qwerty@12',
+        'HOST': 'localhost',  # Set to the host where your PostgreSQL server is running
+        'PORT': '5432',      # Set to the port used by your PostgreSQL server (default is 5432)
     }
 }
 
